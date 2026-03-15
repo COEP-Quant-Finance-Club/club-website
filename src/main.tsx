8ab40b365bb044dfb4f05769d872402fd876cf59
Abhiraj Vaidya <abhivaidya57@gmail.com>
2026-03-15 07:34:43 +0000

    fff

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
+function Root() {
+  try {
+    return <App />;
+  } catch (err) {
+    console.error(err);
+    return <div style={{padding:20}}>App crashed. Check console.</div>;
+  }
+}
+
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
-    <App />
+    <Root />
   </HashRouter>
 );
